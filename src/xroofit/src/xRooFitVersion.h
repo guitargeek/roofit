d8c3e656cb1cba0c657c64673bdfe6d8831b3a70
Jonas Rembser <jonas.rembser@cern.ch>
2024-05-29 01:06:12 +0200

    Sync with ROOT 6.32.02

---
+++ src/xroofit/src/xRooFitVersion.h
@@ -0,0 +1,16 @@
+/*
+* Project: xRooFit
+* Author:
+*   Will Buttinger, RAL 2024
+*
+* Copyright (c) 2024, CERN
+*
+* Redistribution and use in source and binary forms,
+* with or without modification, are permitted according to the terms
+* listed in LICENSE (http://roofit.sourceforge.net/license.txt)
+ */
+
+#pragma once
+
+#define GIT_COMMIT_HASH "32646f9"
+#define GIT_COMMIT_DATE "2024-06-13 17:14:41 +0200"
