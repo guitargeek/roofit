e9a9e5ae8f55dd69d0fecc7515977b010052b86a
Jonas Rembser <jonas.rembser@cern.ch>
2025-01-02 04:41:02 +0100

    Make sure all public headers are installed

--- default.nix
+++ default.nix
@@ -1,18 +1,34 @@
 with import <nixpkgs> { };
 
-clangStdenv.mkDerivation {
+#clangStdenv?
+stdenv.mkDerivation {
   name = "roofit";
   src = ./.;
 
   buildInputs = [
     (root.overrideAttrs (old: { cmakeFlags = old.cmakeFlags ++ [ "-Droofit=OFF -Dtmva=OFF -Dtestsupport=ON" ]; }))
+    ccache
     cmake
     gsl
     gtest
+    libxml2
     nlohmann_json
     pkg-config
   ];
 
   cmakeFlags = [
   ];
+
+  shellHook = ''
+
+    alias configure="cmake \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_INSTALL_PREFIX=../install \
+        -Dccache=ON \
+        -Dtesting=ON \
+        -Dxml=OFF \
+        .."
+
+    alias build="cmake --build . --target install -j8"
+  '';
 }
--- setup.fish
+++ setup.fish
@@ -1,3 +1,3 @@
-set -g ROOT_INCLUDE_PATH $PWD/install/include
-set -g -a LD_LIBRARY_PATH $PWD/install/lib
-set -g -a DYLD_LIBRARY_PATH $PWD/install/lib
+set -g -x ROOT_INCLUDE_PATH $PWD/install/include
+set -g -x -a LD_LIBRARY_PATH $PWD/install/lib
+set -g -x -a DYLD_LIBRARY_PATH $PWD/install/lib
